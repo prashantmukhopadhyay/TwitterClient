4ff85acd703d5892aed86acef5ce5117a381991e
David Park <parkdy6@gmail.com>
2013-11-11 19:03:15 -0500

    Allow posting tweets

--- app/models/status.rb
+++ app/models/status.rb
@@ -41,4 +41,24 @@ class Status < ActiveRecord::Base
     end
   end
 
+  def self.post(text)
+    url = Addressable::URI.new(
+    scheme: 'https',
+    host: 'api.twitter.com',
+    path: '/1.1/statuses/update.json',
+    query_values: { status: text }
+    ).to_s
+
+    json_str = TwitterSession.post(url).body
+    results = JSON.parse(json_str)
+
+    p results
+    tweet_id_str = results["id_str"]
+    body = results["text"]
+    user_id = results["user"]["id_str"]
+
+    Status.create(twitter_status_id: tweet_id_str, twitter_user_id: user_id, body: body)
+
+  end
+
 end
